c621092ff3a16db0aebdc2286a4478a888d23182
Prashant Varma <prashantvarma5083@gmail.com>
2024-07-18 20:43:29 +0530

    init setup

---
+++ Client/src/App.tsx
@@ -0,0 +1,12 @@
+import Button from '@mui/material/Button';
+
+ function App() {
+  return (
+    <div>
+      <span className="text-3xl font-bold mr-5">Hello world!</span>
+       <Button variant="contained">Hello world</Button>
+    </div>
+  )
+}
+
+export default App
